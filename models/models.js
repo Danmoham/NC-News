531dce038610e0eb68c0183db383f6321bfeb6e4
Danmoham <dmoham35@gmail.com>
2023-10-03 12:28:37 +0100

    done

--- models/models.js
+++ models/models.js
@@ -21,7 +21,6 @@ exports.fetchArticleById = (id) =>{
   
 }
 exports.fetchAllArticles = () =>{
-    //comments 404 
     return db.query(`SELECT articles.author,articles.title, articles.article_id, articles.topic, articles.created_at,articles.votes, articles.article_img_url,CAST(count(articles.article_id)as INTEGER) AS comment_count FROM articles LEFT JOIN comments ON articles.article_id = comments.article_id
     GROUP BY articles.article_id ORDER BY articles.created_at DESC`).then(({rows}) =>{
         return rows
